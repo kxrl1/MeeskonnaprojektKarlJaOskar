e24cb29c64a11f1981300e1e009237a0bebb04a7
oskarpadu <oskar.padu@voco.ee>
2026-05-06 10:37:27 +0300

    connect database and sync models

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { sequelize } = require('./models');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,5 +11,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'CineRating API töötab!' });
 });
+
+sequelize.sync({ alter: true })
+  .then(() => console.log('Andmebaas sünkroniseeritud!'))
+  .catch((err) => console.error('Andmebaasi viga:', err));
 
 module.exports = app;
