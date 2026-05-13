1dc729ca45acd4befc866f978a0d48be3994e1cb
oskarpadu <oskar.padu@voco.ee>
2026-05-13 09:47:26 +0300

    feat: added genre and movie posters updated, made movie page, movie big slider 30 s to 10 s

--- backend/src/config/seed.js
+++ backend/src/config/seed.js
@@ -4,6 +4,16 @@ require('dotenv').config();
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
+const GENRES = {
+  28: 'Action', 12: 'Adventure', 16: 'Animation',
+  35: 'Comedy', 80: 'Crime', 99: 'Documentary',
+  18: 'Drama', 10751: 'Family', 14: 'Fantasy',
+  36: 'History', 27: 'Horror', 10402: 'Music',
+  9648: 'Mystery', 10749: 'Romance', 878: 'Sci-Fi',
+  10770: 'TV Movie', 53: 'Thriller', 10752: 'War',
+  37: 'Western'
+};
+
 const fetchAndSeedMovies = async () => {
   try {
     await sequelize.authenticate();
@@ -23,7 +33,7 @@ const fetchAndSeedMovies = async () => {
       releaseYear: m.release_date ? parseInt(m.release_date.split('-')[0]) : null,
       posterUrl: m.poster_path ? `https://image.tmdb.org/t/p/w500${m.poster_path}` : null,
       backdropUrl: m.backdrop_path ? `https://image.tmdb.org/t/p/original${m.backdrop_path}` : null,
-      genre: m.genre_ids.join(', '),
+      genre: m.genre_ids.map(id => GENRES[id] || id).join(', '),
     });
     }
 
